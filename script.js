5bb3d1d700693f8f97150bece2d2b74676e23b52
Joelljunggren <joelljunggren@hotmail.com>
2022-12-06 15:12:45 +0100

    Large changes

--- script.js
+++ script.js
@@ -11,3 +11,43 @@ document.querySelectorAll(".nav-link").forEach(n => n.
         hamburger.classList.remove("active");
         navMenu.classList.remove("active")
     }))
+
+const form = document.getElementById('form')
+const namn = document.getElementById('namn') 
+const medlemsnr = document.getElementById('medlemsnr')
+const bastu = document.getElementById('bastu')
+const datum = document.getElementById('datum')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+})
+
+function validateInputs(){
+    const namnValue = namn.value.trim();
+    const medlemsnrValue = medlemsnr.value.trim();
+    const bastuValue = bastu.value.trim();
+    const datumValue = datum.value.trim();
+
+    if(namnValue == ''){
+        validateError(namn, 'Du måste fylla i ditt namn')
+    }
+    if(medlemsnrValue == ''){
+        validateError(medlemsnr, 'Du måste fylla i ditt medlemsnummer')
+    }
+    if(bastuValue == ''){
+        validateError(bastu, 'Välj bastusort')
+    }
+    if(datumValue == ''){
+        validateError(datum, 'Du måste välja ett bokningsdatum')
+    }
+}
+
+function validateError(input, errorMsg){
+    const inputControl = input.parentElement;
+    inputControl.className = 'input-control failed';
+    const small = inputControl.querySelector('small');
+
+    small.innerText = errorMsg;
+}
